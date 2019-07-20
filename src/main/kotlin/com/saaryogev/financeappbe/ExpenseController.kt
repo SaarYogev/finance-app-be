afa0ceae4ae51d20e6ee7c8d5fdc659a6bf8b294
Saar Yogev <saaryogev@gmail.com>
2019-07-20 22:28:09 +0300

    Fixed add method to use request body

--- src/main/kotlin/com/saaryogev/financeappbe/ExpenseController.kt
+++ src/main/kotlin/com/saaryogev/financeappbe/ExpenseController.kt
@@ -2,13 +2,14 @@ package com.saaryogev.financeappbe
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ExpenseController(private val expenseRepo: ExpenseRepo) {
 
     @PostMapping("/expense")
-    fun addExpense(expense: Expense) {
+    fun addExpense(@RequestBody expense: Expense) {
         expenseRepo.save(expense)
     }
 
