f5aa52a8a9893457f939ed4c57b34ff0912a5ada
Saar Yogev <saaryogev@gmail.com>
2019-08-23 14:48:40 +0300

    Added string parsing

--- src/main/kotlin/com/saaryogev/financeappbe/ExpenseController.kt
+++ src/main/kotlin/com/saaryogev/financeappbe/ExpenseController.kt
@@ -1,5 +1,7 @@
 package com.saaryogev.financeappbe
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -11,6 +13,11 @@ class ExpenseController(private val expenseRepo: ExpenseRepo) {
     @PostMapping("/expense")
     fun addExpense(@RequestBody expense: Expense) {
         expenseRepo.save(expense)
+    }
+
+    @PostMapping("/expenseString")
+    fun addExpense(@RequestBody expense: String) {
+        expenseRepo.save(ObjectMapper().readValue(expense))
     }
 
     @GetMapping("/expense")
