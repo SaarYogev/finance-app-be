940ad961ce511c7f5b53b066a9e06da194bd3ac3
Saar Yogev <saaryogev@gmail.com>
2019-08-23 23:02:41 +0300

    Added clear option

--- src/main/kotlin/com/saaryogev/financeappbe/ExpenseController.kt
+++ src/main/kotlin/com/saaryogev/financeappbe/ExpenseController.kt
@@ -2,10 +2,7 @@ package com.saaryogev.financeappbe
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class ExpenseController(private val expenseRepo: ExpenseRepo) {
@@ -23,5 +20,10 @@ class ExpenseController(private val expenseRepo: ExpenseRepo) {
     @GetMapping("/expense")
     fun getExpenses(): MutableIterable<Expense> {
         return expenseRepo.findAll()
+    }
+
+    @DeleteMapping("/expense")
+    fun clearExpenses() {
+        expenseRepo.deleteAll()
     }
 }
