776bd27ea4acaad0d471f527707a760810435cd8
Saar Yogev <saaryogev@gmail.com>
2019-08-23 21:58:46 +0300

    removed test insertion

--- src/main/kotlin/com/saaryogev/financeappbe/FinanceAppBeApplication.kt
+++ src/main/kotlin/com/saaryogev/financeappbe/FinanceAppBeApplication.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import java.util.*
 
 fun main(args: Array<String>) {
     runApplication<FinanceAppBeApplication>(*args)
@@ -16,7 +15,6 @@ class FinanceAppBeApplication {
     @Bean
     fun demo(repository: ExpenseRepo): CommandLineRunner {
         return CommandLineRunner {
-            repository.save(Expense(2, "3", "4", Date()))
         }
     }
 }
